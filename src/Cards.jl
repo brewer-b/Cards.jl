b915a0a2e7741dbb1ceb1ff6e5bfc04e8e99d725
brewer-b <44099384+brewer-b@users.noreply.github.com>
2023-05-05 11:42:16 +0300

    Add files via upload

--- src/Cards.jl
+++ src/Cards.jl
@@ -1,5 +1,17 @@
 module Cards
 
-# Write your package code here.
+export Card
+
+#Cards are in range 1:52, in ascending order (2c,2d,2h,2s,3c,3d,3h,3s,4c,...)
+struct Card
+    value::Unsigned
+end
+
+function Base.show(io::IO, c::Card)
+    rank = ((c.value - 1) ÷ 4) + 1
+    suit = ((c.value - 1) % 4) + 1
+    print(io, "23456789TJQKA"[rank])
+    print(io, ["♣","♦","♥","♠"][suit])
+end
 
 end
